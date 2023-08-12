85f470b80b025570028fb6b2374e1ad097915e93
Kyungjae Lee <kjxlee@gmail.com>
2023-08-12 03:03:33 +0100

    Create 'Led_Toggling_using_DMA_Polling' and 'Led_Toggling_using_DMA_IT' projects

---
+++ Workspace/Led_Toggling_using_DMA_Polling/MDK-ARM/RTE/_Led_Toggling_using_DMA_Polling/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Led_Toggling_using_DMA_Polling' 
+ * Target:  'Led_Toggling_using_DMA_Polling' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
